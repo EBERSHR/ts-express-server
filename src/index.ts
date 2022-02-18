6857f1fa50783b70d5e1d9d17feec15fd7647e47
Eber Hernández <ebershr@gmail.com>
2022-02-18 06:34:29 -0500

    Restoring server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-const clubs = require('../data/clubs.json');
-console.log(clubs);
+// import createServer from "./server";
+import createServer from "./server";
+
+createServer().listen(3000, () => {
+    console.log('Server running on port 3000');
+});
