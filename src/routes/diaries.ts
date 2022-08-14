67a31fbb0fd42b5edc07a973892989ca0c2173ac
EBERSHR <ebershr@gmail.com>
2022-08-13 20:46:56 -0500

    Revisando y adaptando

---
+++ src/routes/diaries.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (_req, res) => {
+
+    res.send('Fetchin all entry diaries');
+
+    res.send('pong');
+});
+
+router.post('/', (_req, res) => {
+    res.send('Saving a diary');
+}  
+);
+
+export default router;
